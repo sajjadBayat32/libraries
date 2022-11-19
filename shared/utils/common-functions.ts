4e7f24dd9c0611132e36273ec7d6f1f1ff681df0
sajjadBayat32 <sajjad.bayat32@gmail.com>
2022-11-19 22:51:04 +0330

    copy to clipboard function added to common functions

--- shared/utils/common-functions.ts
+++ shared/utils/common-functions.ts
@@ -1,11 +1,31 @@
 import {Injectable} from "@angular/core";
+import {NzMessageService} from "ng-zorro-antd/message";
 
 @Injectable()
 export class CommonFunctions {
+
+  constructor(private toaster: NzMessageService) {
+  }
+
   groupJsonList<T>(list: T[], key: string) {
     return list.reduce(function(rv: any, x: any) {
       (rv[x[key]] = rv[x[key]] || []).push(x);
       return rv;
     }, {});
   };
+
+  copyToClipboard(value: string) {
+    let selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = value;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+    this.toaster.info('کپی شد');
+  }
 }
